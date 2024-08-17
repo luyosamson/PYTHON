7d07031b8472c18b777941d074c0b240b7d27599
luyosamson <luyosamson@gmail.com>
2024-08-17 13:29:53 +0300

    Added module folder

---
+++ Modules/greeting.py
@@ -0,0 +1,8 @@
+def myname(name):
+    return name
+
+myname("Luyo")
+
+def add(a,b):
+    return a+b
+# print(add(20,20))
---
+++ Modules/mathmodule.py
@@ -0,0 +1,5 @@
+import greeting as g
+m=g.add(90,100)
+print(m)
+n=g.myname("Luyo")
+print(f"My name is {n}")
